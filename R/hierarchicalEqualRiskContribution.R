fa275096df001891bf5796750abc2b1d6bf91989
Jacky Lau <gmsbroa@gmail.com>
2020-09-12 13:19:07 +0800

    added alternative risk metrics to HERC

--- R/hierarchicalEqualRiskContribution.R
+++ R/hierarchicalEqualRiskContribution.R
@@ -7,7 +7,7 @@ computeClusterInertia <- function(labels, asset_returns) {
     inertia <- inertia + sum(dist(asset_returns[,labels==label])) * 2 / n^2
   }
   return(log(inertia))
-}
+} 
 
 computeExpectedInertia <- function(N, D, num_clusters, linkage, num_reference_datasets=5) {
   reference_inertia <- 0
@@ -16,9 +16,13 @@ computeExpectedInertia <- function(N, D, num_clusters, linkage, num_reference_da
     reference_corr <- cor(reference_asset_returns)
     reference_dist <- as.dist(sqrt(2 * (1 - reference_corr)))
 
-    reference_cluster_assignments <- dendextend::cutree(hclust(reference_dist, method=linkage), k=num_clusters, order_clusters_as_data=F)
+    reference_cluster_assignments <- dendextend::cutree(hclust(reference_dist,
+                                                               method=linkage),
+                                                        k=num_clusters,
+                                                        order_clusters_as_data=F)
 
-    inertia <- computeClusterInertia(reference_cluster_assignments, reference_asset_returns)
+    inertia <- computeClusterInertia(reference_cluster_assignments,
+                                     reference_asset_returns)
     reference_inertia <- reference_inertia + inertia
   }
   return(reference_inertia/num_reference_datasets)
@@ -33,7 +37,10 @@ computeNumClusters <- function(rho, linkage, asset_returns, distance) {
   original_clusters <- hclust(distance, method=linkage)
 
   for(i in 1:nrow(rho)){
-    original_cluster_assignments <- dendextend::cutree(original_clusters, k=num_clusters, order_clusters_as_data=F)
+    original_cluster_assignments <- dendextend::cutree(original_clusters,
+                                                       k=num_clusters,
+                                                       order_clusters_as_data=F)
+
     if(max(original_cluster_assignments)==max_clusters |
        max(original_cluster_assignments)>10){
       break
@@ -55,6 +62,22 @@ getIVP <- function(Sigma) {
   return(ivp)
 }
 
+getInverseCVaR <- function(asset_returns) {
+  if(ncol(asset_returns)==1) return(1)
+  w <- PerformanceAnalytics::CVaR(asset_returns, method="historical")
+  w <- 1/w
+  w <- w / sum(w)
+  return(w)
+}
+
+getInverseCDaR <- function(asset_returns) {
+  if(ncol(asset_returns)==1) return(1)
+  w <- PerformanceAnalytics::CDD(asset_returns)
+  w <- 1/w
+  w <- w / sum(w)
+  return(w)
+}
+
 getClusterVariance <- function(Sigma, cluster_idx) {
   Sigma_ <- Sigma[cluster_idx, cluster_idx]
   w_ <- getIVP(Sigma_)
@@ -62,6 +85,39 @@ getClusterVariance <- function(Sigma, cluster_idx) {
   return(cluster_var[1])
 }
 
+getClusterCVaR <- function(asset_returns, cluster_idx) {
+  conf <- 0.05
+  cluster_returns <- asset_returns[,cluster_idx]
+
+  w <- getInverseCVaR(cluster_returns)
+
+  # portfolio returns is then an xts
+  portfolio_returns <- w %*% t(cluster_returns)
+  portfolio_returns <- t(portfolio_returns)
+
+  cluster_cvar <- PerformanceAnalytics::CVaR(portfolio_returns, p=conf, method="historical")
+
+  # weights doesn't seem to work for PerformanceAnalytics
+  # cluster_cvar <- PerformanceAnalytics::CVaR(cluster_returns, weights=w, p=conf, method="historical")
+  return(cluster_cvar[1])
+}
+
+getClusterCDaR <- function(asset_returns, cluster_idx) {
+  conf <- 0.05
+  cluster_returns <- asset_returns[,cluster_idx]
+
+  w <- PerformanceAnalytics::CDD(cluster_returns, p=conf)
+  w <- 1/w
+  w <- w / sum(w)
+
+  # portfolio returns is then an xts
+  portfolio_returns <- w %*% t(cluster_returns)
+  portfolio_returns <- t(portfolio_returns)
+
+  cluster_cdd <- PerformanceAnalytics::CDD(portfolio_returns, p=conf)
+
+  return(cluster_cdd[[1]])
+}
 
 calculateClusterRiskContribution <- function(risk_measure, Sigma, asset_returns, 
                                              num_clusters, cluster_assignments){
@@ -69,49 +125,83 @@ calculateClusterRiskContribution <- function(risk_measure, Sigma, asset_returns,
 
   for(i in 1:num_clusters){
     cluster_asset_ind <- which(cluster_assignments==i)
-    if(risk_measure=='variance'){
-      cluster_contributions[i] <- getClusterVariance(Sigma, cluster_asset_ind)
-    }
+
+    switch(risk_measure,
+           'variance'={
+             cluster_contributions[i] <- getClusterVariance(Sigma, cluster_asset_ind)
+           },
+           'standard-deviation'={
+             cluster_contributions[i] <- sqrt(getClusterVariance(Sigma, cluster_asset_ind))
+           },
+           'CVaR'={
+             cluster_contributions[i] <- getClusterCVaR(asset_returns, cluster_asset_ind)
+           },
+           'CDaR'={
+             cluster_contributions[i] <- getClusterCDaR(asset_returns, cluster_asset_ind)
+           }
+    )
   }
   return(cluster_contributions)
 }
 
-getRecursiveBisection <- function(dend, cluster_contributions){
+getRecursiveBisection <- function(dend, cluster_contributions, risk_measure){
   num_clusters <- nobs(dend)
   weights <- rep(1,num_clusters)
 
-  computeChildClusterWeights <- function(dend, cluster_contributions) {
+  computeChildClusterWeights <- function(dend, cluster_contributions, risk_measure) {
     left <- dend[[1]]
     right <- dend[[2]]
-
     left_order <- order.dendrogram(left)
     right_order <- order.dendrogram(right)
-    left_contrib <- sum(cluster_contributions[left_order])
-    right_contrib <- sum(cluster_contributions[right_order])
 
-    alpha <- right_contrib /(right_contrib+left_contrib)
+    if(risk_measure=='equal-weighting'){
+      alpha <- 0.5
+    }else {
+      left_contrib <- sum(cluster_contributions[left_order])
+      right_contrib <- sum(cluster_contributions[right_order])
+
+      alpha <- right_contrib /(right_contrib+left_contrib)
+    }
+
     weights[left_order] <<- weights[left_order] * alpha
     weights[right_order] <<- weights[right_order] * (1-alpha)
 
     if(nobs(left)>1)
-      computeChildClusterWeights(left, cluster_contributions)
+      computeChildClusterWeights(left, cluster_contributions, risk_measure)
     if(nobs(right)>1)
-      computeChildClusterWeights(right, cluster_contributions)
+      computeChildClusterWeights(right, cluster_contributions, risk_measure)
   }
-  computeChildClusterWeights(dend, cluster_contributions)
+  computeChildClusterWeights(dend, cluster_contributions, risk_measure)
   return(weights)
 }
 
 
 calculateFinalPortfolioWeights <- function(risk_measure, clusters_weights, Sigma,
-                                           num_clusters, cluster_assignments) {
+                                           asset_returns, num_clusters, cluster_assignments) {
   w <- rep(1, nrow(Sigma))
   for(i in 1:num_clusters){
     cluster_inds <- cluster_assignments==i
     Sigma_ <- Sigma[cluster_inds, cluster_inds]
-    #cluster_asset_returns <- asset_returns[,cluster_inds]
+    cluster_asset_returns <- asset_returns[,cluster_inds]
 
-    parity_weights <- getIVP(Sigma_)
+    switch(risk_measure,
+           'variance'={
+             parity_weights <- getIVP(Sigma_)
+           },
+           'standard-deviation'={
+             parity_weights <- getIVP(Sigma_)
+           },
+           'equal-weighting'={
+             n <- sum(cluster_assignments==i)
+             parity_weights <- rep(1/n, n)
+           },
+           'CVaR'={
+             parity_weights <- getInverseCVaR(cluster_asset_returns)
+           },
+           'CDaR'={
+             parity_weights <- getInverseCDaR(cluster_asset_returns)
+           }
+    )
 
     w[cluster_inds] <- parity_weights * clusters_weights[i]
   }
@@ -130,10 +220,10 @@ getPortfolioWeights <- function(hcluster, asset_returns, Sigma, num_clusters,
   dend_depth <- hcluster$height[N - num_clusters]
   clusters_dend <- cut(as.dendrogram(hcluster), h=dend_depth)$upper
 
-  clusters_weights <- getRecursiveBisection(clusters_dend, cluster_contributions)
+  clusters_weights <- getRecursiveBisection(clusters_dend, cluster_contributions, risk_measure)
 
   w <- calculateFinalPortfolioWeights(risk_measure, clusters_weights, Sigma, 
-                                      num_clusters, cluster_assignments)
+                                      asset_returns, num_clusters, cluster_assignments)
 
   names(w) <- names(asset_returns)
   return(w)
@@ -166,3 +256,4 @@ hierarchicalEqualRiskContribution <- function(asset_prices,
 
   return(list('w'=w))
 }
+
